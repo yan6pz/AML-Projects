587790d5b1b82420e498300c8d168131a5567383
Yan <shterev2@illinois.edu>
2019-02-03 23:42:09 -0600

    Change in the formula

--- homework3/PCA_iris.py
+++ homework3/PCA_iris.py
@@ -14,7 +14,7 @@ def PCA(dataset, n_components):
     #print(col_mean)
     # center columns by subtracting column means
     
-    C = dataset - col_mean
+    C = dataset 
     
     cov_mat = (C - col_mean).T.dot((C - col_mean)) / (C.shape[0]-1)
     
@@ -49,13 +49,33 @@ def MSE(dataset, component_mat):
     #print(pd.DataFrame(dataset).tail())
     #print(pd.DataFrame(component_mat).tail())
     
-    if dataset.shape[1] > component_mat.shape[1]:
-        zeros = np.zeros((dataset.shape[1] - component_mat.shape[1] , component_mat.shape[0]))
-        component_mat = np.concatenate((component_mat, zeros.T), axis=1)
+    #if dataset.shape[1] > component_mat.shape[1]:
+   #     zeros = np.zeros((dataset.shape[1] - component_mat.shape[1] , component_mat.shape[0]))
+    #    component_mat = np.concatenate((component_mat, zeros.T), axis=1)
     
     mse = (np.square(dataset - component_mat)).mean(axis=0)
     return mse
     
+def reconstruct(u, mean, data):
+    
+    mean = np.resize(mean,(noisy_data[i].shape[0], noisy_data[i].shape[1]))
+
+    ri = np.dot((data - mean), u) #ri = UT mi = UT(xi − mean ({x})).
+    pi = ri
+    if 4 > ri.shape[1]:
+        zeros = np.zeros((4 - ri.shape[1] , ri.shape[0]))
+        pi = np.concatenate((ri, zeros.T), axis=1)
+    #xˆi = Upi + mean ({x})
+
+    #unrotate and untranslate adding mean: xˆi = Upi + mean ({x})
+    xi = np.dot(pi, u)
+    #reshape xi to have the shape of the original data
+    if 4 > xi.shape[1]:
+        zeros = np.zeros((4 - xi.shape[1] , xi.shape[0]))
+        xi = np.concatenate((xi, zeros.T), axis=1)
+
+    xi += mean
+    return xi
    
 N = np.array(pd.read_csv('iris.csv', na_values=" ?"))
 data1 = np.array(pd.read_csv('dataI.csv', na_values=" ?"))
@@ -91,17 +111,13 @@ for i in range(len(noisy_data)):
         #do not dot if it is 0 PC
         if j != 0:
             #reconstructed version on noisy dataset i using PC of noiseless data j
-            pc, mean = noiseless_data_PCs[j]
-            pca_representation = noisy_data[i].dot(pc)
-            #unrotate and untranslate adding mean
-            
-            pca_representation = np.dot( pca_representation, pc.T)
-            pca_representation += mean
+            u, mean = noiseless_data_PCs[j]
+            xi = reconstruct(u, mean, noisy_data[i])
         else:
-            pca_representation = noiseless_data_PCs[j]
+            xi = N0
             
         #MSE between the noiseless version N and the pca representation
-        mse.append(sum(MSE(N, pca_representation)))
+        mse.append(sum(MSE(N, xi)))
        
     
     #noisy  
@@ -109,20 +125,18 @@ for i in range(len(noisy_data)):
     for k in range(5):
         
         if k != 0:
-            pc_k_noisy, mean = PCA(noisy_data[i], k)
+            u, mean = PCA(noisy_data[i], k)
             #reconstructed version on noisy dataset i using k PC computed from
             # mean and covmat of the same set
-            pca_representation = noisy_data[i].dot(pc_k_noisy)
-            pca_representation = np.dot( pca_representation, pc_k_noisy.T)
-            pca_representation += mean
+            xi = reconstruct(u, mean, noisy_data[i])
             if i == 1 and k==2:
-                data1_2pc_representation = pca_representation
+                data1_2pc_representation = xi
         else:
-            pca_representation = np.mean(noisy_data[i], axis=0)
-            pca_representation = np.resize(pca_representation,(noisy_data[i].shape[0], noisy_data[i].shape[1]))
+            xi = np.mean(noisy_data[i], axis=0)
+            xi = np.resize(xi,(noisy_data[i].shape[0], noisy_data[i].shape[1]))
             
         #MSE between the noisy version i and the k-th pca representation of 
-        mse.append(sum(MSE(noisy_data[i], pca_representation)))
+        mse.append(sum(MSE(noisy_data[i], xi)))
     
     mse_matrix.append(mse)
 
@@ -136,8 +150,8 @@ data1_2pc_representation = np.around(data1_2pc_representation, decimals=2)
                                      
 print(mse_matrix)
 
-np.savetxt("shterev2-numbers_unrotated_untranslated.csv", mse_matrix, delimiter=",", fmt='%5.2f')
-np.savetxt("shterev2-recon_unrotated_untranslated.csv", data1_2pc_representation, delimiter=",", fmt='%5.2f')
+np.savetxt("shterev2-numbers_unrotated_untranslated1.csv", mse_matrix, delimiter=",", fmt='%5.2f')
+np.savetxt("shterev2-recon_unrotated_untranslated1.csv", data1_2pc_representation, delimiter=",", fmt='%5.2f')
 
 
  
