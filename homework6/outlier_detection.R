5cdc2c43dd42f57e00acf1b249e2991b9028a6d4
Yanislav Shterev <Yanilsav.Shterev@partner.bmwgroup.com>
2019-03-02 13:03:13 -0600

    HW6 Wrapping outlier detection. Starting the report

--- homework6/outlier_detection.R
+++ homework6/outlier_detection.R
@@ -1,34 +1,91 @@
 library(MASS)
-library(ISLR)
+library(car)
 
-setwd("D:/Masters/MCS-DS/Applied machine learning/Homeworks/AML-Projects/homework6")
+#setwd("~/Desktop/AML/AML-Projects/homework6")
 names <- c("crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","black","lstat","medv")
 data <- read.table("housing.data", header=F, col.names = names)
 
 summary(data)
 head(data)
 
-#regress only over the black variable
-lr_against_black=lm(medv~black,data=data)
-summary(lr_against_black)
-par(mfrow=c(2,2))
-plot(lr_against_black)
+estimateStatistics <- function(regression_model){
+  #1 Estimate R2
+  rsq<-summary(regression_model)$r.squared 
+  print("R-squared: ")
+  print(rsq)
+  #2 Estimate residuals
+  residuals <- residuals(regression_model)
+  print("Residuals: ")
+  print(residuals)
+  #3 Estimate the hat matrix leverage
+  leverage <- hatvalues(regression_model)
+  print("Leverage: ")
+  print(leverage)
+  #4 Estimate cook distance
+  # identify D values > 4/(n-k-1) 
+  cutoff <- 4/((nrow(data)-length(regression_model$coefficients)-2)) 
+  print("Cutoff: ")
+  print(cutoff)
+  plot(regression_model, which=4, cook.levels=cutoff)
+  # Influence Plot 
+  #influencePlot(regression_model, id.method="identify", main="Influence Plot", sub="Circle size is proportial to Cook's Distance" )
+  
+  return(T)
+}
 
-plot(medv~black,data)
-abline(lr_against_black,col="red")
+plot_summarize <- function(regression_model){
+  summary(regression_model)
+  par(mfrow=c(2,2))
+  plot(regression_model)
+  par(mfrow=c(1,1))
+}
 
+regress <- function(data){
+  multi_lr=lm(medv~crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat,data)
+  estimateStatistics(multi_lr)
+  
+  #resulting model
+  plot_summarize(multi_lr)
+  
+  return(multi_lr)
+}
 
 
 #regress over all the attributes
-multi_lr=lm(medv~crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat,data)
-summary(multi_lr)
-plot(multi_lr)
+regress(data)
+
+#remove top 3 influential points
+data_without_3_outliers <- data[-c(365, 369, 373), ]
+
+regress(data_without_3_outliers)
+
+#remove top 6 influential points
+data_without_6_outliers <- data[-c(366, 368, 370,365, 369, 373), ]
+
+regress(data_without_6_outliers)
+
+#remove top 10 influential points
+data_without_10_outliers <- data[-c(366, 368, 370,365, 369, 373,372,371,381,413), ]
+
+model_cleaned <- regress(data_without_10_outliers)
+
+#Box-Cox transformation choosing lambda
+bc<-boxcox(model_cleaned,lambda=seq(0,1,by=.1))
+best_lambda <- bc$x[which.max(bc$y)]
+print("Best Lambda: ")
+print(best_lambda)
+
+#transform the model based on box-cox's lambda
+transformed_model <- lm(((medv^best_lambda-1)/best_lambda) ~ crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat,data_without_10_outliers)
+
+#Standardized Residuals vs the Fitted values
+st_residuals <- rstandard(transformed_model)
+plot(st_residuals,data_without_10_outliers$medv,
+     xlab="Standardized Residuals",ylab="Fitted values",main='Standardized Residuals vs the Fitted values')
+
+#Predicted vs Actual values plot
+plot(predict(transformed_model),data_without_10_outliers$medv,
+     xlab="predicted",ylab="actual", main="Predicted vs Actual values")
 
 
-#remove 3 outliers
-data_without_outliers <- data[-c(365, 369, 373), ]
-
-multi_lr_no_outliers=lm(medv~crim+zn+indus+chas+nox+rm+age+dis+rad+tax+ptratio+black+lstat,data_without_outliers)
-summary(data_without_outliers)
-plot(multi_lr_no_outliers)
 
