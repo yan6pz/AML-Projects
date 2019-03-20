04d4822f5076cdb90bd487a9297c416dde3cfa43
Yanislav Shterev <Yanilsav.Shterev@partner.bmwgroup.com>
2019-03-19 22:37:07 -0500

    Cosine distances, log regression binary classification, ROC curve

--- homework7/yelp_sentiment_analysis.py
+++ homework7/yelp_sentiment_analysis.py
@@ -1,22 +1,25 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import nltk
-from nltk.corpus import stopwords
 
 from string import punctuation
 from operator import itemgetter
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
-
-def plot_word_distribution(occurrences):
+def plot_word_distribution(occurrences, title):
     rank = range(len(occurrences))
     plt.figure()
     plt.scatter(rank, occurrences)
     plt.xlabel('Word Rank')
     plt.ylabel('Word Count')
     plt.grid()
+    plt.savefig("distribution_" + title)
     plt.show()
 
 
@@ -73,6 +76,29 @@ def print_nearest_neibors(content, distance):
     print(len(text.split(' ')))
     print("%f %s" % (distance, text))
 
+
+def get_n_neighbors(data, search_query, n):
+    X_new_doc = vectorizer.transform(search_query)
+    neigh = NearestNeighbors(n_neighbors=n, metric='cosine')
+    neigh.fit(data)
+    distances, indexes = neigh.kneighbors(X_new_doc, return_distance=True)
+
+    return distances, indexes
+
+def plot_roc_curve(fpr, tpr, log_roc_auccuracy):
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC Curve (area = %0.2f)' % log_roc_auccuracy)
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc="lower right")
+    plt.savefig('ROC_Curve')
+    plt.show()
+
+
 yelp_dataset = pd.read_csv('yelp_2k.csv')
 
 
@@ -85,18 +111,20 @@ y = yelp_dataset['stars']
 
 ##################################################################################
 
-#words_dictionary = word_count(X, None, {})
-#plot_word_distribution(words_dictionary.values())
+#Initial word distribbution plot
+words_dictionary = word_count(X, None, {})
+plot_word_distribution(words_dictionary.values(), "initial")
 
-
+#plot first 500 word distribution and oserve the curve
 #words_dictionary_filtered = word_count(X, 500, {})
 #plot_word_distribution(words_dictionary_filtered.values())
 
-
+#apply min and max document frequency and remove stop words
 stop_words, vectorizer, training_set = max_min_document_frequency(X)
 words_dictionary_preprocessed = word_count(X, None, stop_words, False)
-plot_word_distribution(words_dictionary_preprocessed.values())
+plot_word_distribution(words_dictionary_preprocessed.values(), "stop_words_removed")
 
+#construct bag of words representation
 #print(X.head())
 bag_of_words = []
 counter = 0
@@ -116,27 +144,40 @@ for review in X:
 
 n = 5
 docs_new = ['Horrible customer service']
-X_new_doc = vectorizer.transform(docs_new)
 
-from sklearn.neighbors import NearestNeighbors
-neigh = NearestNeighbors(n_neighbors=n, metric='cosine')
-neigh.fit(training_set)
-distances, indexes = neigh.kneighbors(X_new_doc, return_distance=True)
+#find first 5 neighors
+distances, indexes = get_n_neighbors(training_set, docs_new, n)
 count = 0
-
 for i in indexes[0]:
     print_nearest_neibors(X.iloc[i], distances[0][count])
-    count +=1
+    count += 1
 
-'''
-yelp_dataset['text length'] = yelp_dataset['text'].apply(len)
-print(yelp_dataset.head())
-
-
+#find all of the distances. After the 700th we see all the distances are 1(no similarity at all)
+distances, indexes = get_n_neighbors(training_set, docs_new, 2000)
+plot_word_distribution(distances[0], "cosine_distances")
 
 
-one_star_reviews = yelp_dataset[(yelp_dataset['stars'] == 1)]
-five_star_reviews = yelp_dataset[(yelp_dataset['stars'] == 5)]
-plot_histograms(one_star_reviews)
-plot_histograms(five_star_reviews)
-'''
+##################################################################################
+
+#Part 3: Classification with Logistic Regression
+
+##################################################################################
+
+
+#split into train and test sets. Fit Log Reg model
+X_train, X_test, y_train, y_test = train_test_split(training_set, y, test_size=0.1, random_state=1)
+log_regression = LogisticRegression(random_state=0, solver='liblinear',
+                         multi_class='ovr').fit(X_train, y_train)  #liblinear for small binary problems
+
+#predit and get accuracy
+predictions = log_regression.predict(X_test)
+print(confusion_matrix(y_test, predictions))
+print('\n')
+print(classification_report(y_test, predictions)) #reaching 92% accuracy
+
+
+#ROC Curve
+log_roc_auccuracy = roc_auc_score(y_test, log_regression.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test.values, log_regression.predict_proba(X_test)[:,1], pos_label=5)
+
+plot_roc_curve(fpr,tpr,log_roc_auccuracy)
